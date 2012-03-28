5f4ae547dd68121e3e83c677c2b0bc1f03e0ee9a
keithhedger <keithhedger@ce3ec29c-c337-42cb-bc8f-a72e3bd640e3>
2012-03-28 20:00:58 +0000

    git-svn-id: file:///media/LinuxData/Development64/SVN/Xfce-Theme-Manager@125 ce3ec29c-c337-42cb-bc8f-a72e3bd640e3

--- gtkpreview/main.cpp
+++ gtkpreview/main.cpp
@@ -601,44 +601,92 @@ void makecursor(char* theme,char* outPath)
 	cairo_surface_destroy(surface);
 	cairo_destroy(cr);
 }
+/*
 
-void makeicon(char* theme,char* outPath)
-{
 	GdkPixbuf*	arrow;
-	GdkPixbuf*	move;
-	GdkPixbuf*	wait;
-	GdkPixbuf*	hand;
+
+	GtkIconTheme*	theme;
+	
+	int		lsegwid,rsegwid,boxwid,hiteoffset=0;
+	int		closewid=0,maxwid=0,minwid=0,menuwid=0;
+	int		closehite=0,maxhite=0,minhite=0,menuhite=0;
+	int		topleftwid=0,toplefthite=0,topritewid=0,topritehite=0;
+	int		bottomleftwid=0,bottomlefthite=0,bottomritewid=0,bottomritehite=0;
+	int		leftsidewid=0,leftsidehite=0,ritesidewid=0,ritesidehite=0;
+	int		bottomwid=0,bottomhite=0;
+
+	int		title1wid=0,title1hite=0;
+	int		title2wid=0,title2hite=0;
+	int		title3wid=0,title3hite=0;
+	int		title4wid=0,title4hite=0;
+	int		title5wid=0,title5hite=0;
+
+	int		padwid=PADWIDTH;
+
 	cairo_surface_t *surface;
 	cairo_t *cr;
+
+						theme=gtk_icon_theme_new();
+						gtk_icon_theme_set_custom_theme(theme,icontheme);
+						arrow=gtk_icon_theme_load_icon(theme,"user-home",ICONWIDTH,GTK_ICON_LOOKUP_FORCE_SIZE,NULL);
+						if (arrow!=NULL)
+							{
+								gdk_cairo_set_source_pixbuf(cr,arrow,boxwid-ritesidewid-(ICONWIDTH*2)-(ICONPAD*2),title3hite+ICONPAD);
+								cairo_paint_with_alpha(cr,100);
+								g_object_unref(arrow);
+							}
+						if(theme!=NULL)
+							g_object_unref(theme);
+
+*/
+void makeicon(char* themename,char* outPath)
+{
+	GdkPixbuf*	home;
+	GdkPixbuf*	folder;
+	GdkPixbuf*	computer;
+	GdkPixbuf*	drive;
+	cairo_surface_t	*surface;
+	cairo_t		*cr;
+	GtkIconTheme*	theme=NULL;
 
 	gtkPixbuf=NULL;
 	surface=cairo_image_surface_create(CAIRO_FORMAT_ARGB32,128,32);
 	cr=cairo_create(surface);
 
-	arrow=cursorprev("left_ptr",theme);
-	move=cursorprev("fleur",theme);
-	wait=cursorprev("watch",theme);
-	hand=cursorprev("hand2",theme);
+	theme=gtk_icon_theme_new();
+	gtk_icon_theme_set_custom_theme(theme,themename);
 
-	if (arrow==NULL || move==NULL || wait==NULL || hand==NULL)
-		exit(1);
-	cairo_save (cr);
-		gdk_cairo_set_source_pixbuf(cr,arrow,0,0);
-		cairo_paint_with_alpha(cr,100);
-		gdk_cairo_set_source_pixbuf(cr,move,32,0);
-		cairo_paint_with_alpha(cr,100);
-		gdk_cairo_set_source_pixbuf(cr,wait,64,0);
-		cairo_paint_with_alpha(cr,100);
-		gdk_cairo_set_source_pixbuf(cr,hand,96,0);
-		cairo_paint_with_alpha(cr,100);
-	cairo_restore (cr);
+	home=gtk_icon_theme_load_icon(theme,"user-home",ICONWIDTH,GTK_ICON_LOOKUP_FORCE_SIZE,NULL);
+	folder=gtk_icon_theme_load_icon(theme,"folder",ICONWIDTH,GTK_ICON_LOOKUP_FORCE_SIZE,NULL);
+	computer=gtk_icon_theme_load_icon(theme,"user-desktop",ICONWIDTH,GTK_ICON_LOOKUP_FORCE_SIZE,NULL);
+	drive=gtk_icon_theme_load_icon(theme,"drive-harddisk",ICONWIDTH,GTK_ICON_LOOKUP_FORCE_SIZE,NULL);
+
+	if(home!=NULL)
+		{
+			gdk_cairo_set_source_pixbuf(cr,home,0,0);
+			cairo_paint_with_alpha(cr,100);
+			g_object_unref(home);
+		}
+	if(folder!=NULL)
+		{
+			gdk_cairo_set_source_pixbuf(cr,folder,32,0);
+			cairo_paint_with_alpha(cr,100);
+			g_object_unref(folder);
+		}
+	if(computer!=NULL)
+		{
+			gdk_cairo_set_source_pixbuf(cr,computer,64,0);
+			cairo_paint_with_alpha(cr,100);
+			g_object_unref(computer);
+		}
+	if(drive!=NULL)
+		{
+			gdk_cairo_set_source_pixbuf(cr,drive,96,0);
+			cairo_paint_with_alpha(cr,100);
+			g_object_unref(drive);
+		}
 
 	cairo_surface_write_to_png(surface,outPath);
-
-	g_object_unref(arrow);
-	g_object_unref(move);
-	g_object_unref(wait);
-	g_object_unref(hand);
 
 	cairo_surface_destroy(surface);
 	cairo_destroy(cr);
@@ -689,6 +737,7 @@ void getmetafile(char* folder)
 //gtkprev [controls] gtkthemename /out/path/to/png
 //gtkprev [theme] gtkthemename /path/to/border /out/path/to/png
 //gtkprev [cursor] cursortheme /out/path/to/png
+//gtkprev [icon] icontheme /out/path/to/png
 //gtkprev [custom] gtkthemename cursortheme icontheme /path/to/border /out/path/to/png
 
 int main(int argc,char **argv)
@@ -762,6 +811,12 @@ int main(int argc,char **argv)
 	if (strcasecmp(argv[1],"cursor")==0 && argc==4)
 		{
 			makecursor(argv[2],argv[3]);
+			return(0);
+		}
+
+	if (strcasecmp(argv[1],"icon")==0 && argc==4)
+		{
+			makeicon(argv[2],argv[3]);
 			return(0);
 		}
 
