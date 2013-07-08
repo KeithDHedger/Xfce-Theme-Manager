55e440aa45c373ca8ddf60b97ed716086a23e39c
keithhedger <keithhedger@ce3ec29c-c337-42cb-bc8f-a72e3bd640e3>
2013-07-08 13:59:00 +0000

    added polish
    git-svn-id: file:///media/LinuxData/Development64/SVN/Xfce-Theme-Manager@569 ce3ec29c-c337-42cb-bc8f-a72e3bd640e3

--- Xfce-Theme-Manager/src/main.cpp
+++ Xfce-Theme-Manager/src/main.cpp
@@ -508,7 +508,7 @@ void doAbout(GtkWidget* widget,gpointer data)
 	const char*	authors[]={"K.D.Hedger <"MYEMAIL">",NULL};
 	const char	copyright[] ="Copyright \xc2\xa9 2012-2013 K.D.Hedger";
 	const char*	aboutboxstring=_translate(ABOUTBOX);
-	const char*	translators="Spanish translation:\nPablo Morales Romero <pg.morales.romero@gmail.com>.\n\nGerman translation:\nMartin F. Schumann. <mfs@mfs.name>";
+	const char*	translators="Spanish translation:\nPablo Morales Romero <pg.morales.romero@gmail.com>.\n\nGerman translation:\nMartin F. Schumann. <mfs@mfs.name>\n\nPolish translation:\nMichał Olber (OSWorld) <redakcja@osworld.pl> ";
 
 	gtk_show_about_dialog(NULL,"authors",authors,"translator-credits",translators,"comments",aboutboxstring,"copyright",copyright,"version",VERSION,"website",MYWEBSITE,"program-name","Xfce-Theme-Manager","logo-icon-name","xfce-theme-manager",NULL); 
 }
